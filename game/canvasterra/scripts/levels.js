bd9c54db927756b360e935f3adec5e5ec27e4d31
Miss-Terra <ms.terra.h@gmail.com>
2016-05-04 20:39:05 -0700

    Javascript line/level fuctions

---
+++ game/canvasterra/scripts/levels.js
@@ -0,0 +1,75 @@
+//The current level. (level 0 == tutorial)
+var currentLevel = 1;
+//How large the line is. (How many people are in it).
+var lineSize = 0;
+//How many item types are available for the current level.
+//If 1... Then perhaps only HATS are used.
+//If 2... Then only HATS and maybe SHOES are used.
+var itemTypes = 1;
+//How many different unique items are allowed...
+//2 would mean a person can have either a red hat or a blue shirt assigned.
+//Perhaps they have 1 or both.
+//(type/color) define a unique item.
+var itemVariety = 2;
+
+//How fast the line moves.
+var speed = 1.0;
+
+var MAXLEVEL = 34; 
+var MAXTYPES = 6; //Hat, shirt, pants, shoes, item1, item2
+
+
+setSettings();
+
+//When loading a level... This sets up the difficulty & level settings.
+function setSettings(){
+	setLineSize();
+	setItemTypes();
+	setItemVariety();
+}
+
+//This is how long the line is.
+function setLineSize(){
+	lineSize = 30;
+}
+
+//Sets the number of item types allowed.
+//Basically this adds 1 item type per 5 levels. (except at the start).
+function setItemTypes(){
+	if (currentLevel <= 2){
+		itemTypes = 1; 
+	}
+	if (currentLevel < 5 && currentLevel > 2){
+		itemTypes = 2;
+	}
+	if (currentLevel < 10 && currentLevel > 4){
+		itemTypes = 3;
+	}
+	if (currentLevel < 15 && currentLevel > 9){
+		itemTypes = 4;
+	}
+	if (currentLevel < 20 && currentLevel > 14){
+		itemTypes = 5;
+	}
+	if (currentLevel >= 20){
+		itemTypes = 6;
+	}
+
+}
+
+//Baiscally this sets item variety.
+//At level 2, you have a max of 2 different objects. (type/color)
+function setItemVariety(){
+
+	if (currentLevel <= 2){
+		itemVariety = 2; 
+	} else{
+		itemVariety = (currentLevel / 2) + 2;
+	}
+
+}
+
+function nextLevel(){
+	level++;
+	setSettings();
+}
---
+++ game/canvasterra/scripts/personGen.js
@@ -0,0 +1,41 @@
+
+//Generate a list of people
+function genPeople(n){
+
+	var person = [];
+
+	for (i = 0; i < n; i++){
+		person[i] = genPerson();
+	}
+	return person;
+}
+
+//Create and return a person.
+function genPerson(){
+	var person = {
+		hat: 0,
+		shirt: 0,
+		item1: 0,
+		item2: 0,
+		pants: 0,
+		shoes: 0,
+
+		//This function assigns items to the person based on the item matrix.
+		//For example, 2 for hat might be a blue sillyhat.
+		//Or 2 for shirt could be a red t-shirt.
+		setItems: function() {
+			this.hat = 2;
+			this.shirt = 2;
+			this.item1 = 2;
+			this.item2 = 2;
+			this.pants = 2;
+			this.shoes = 2;
+		}
+	};
+	person.setItems();
+	return person;
+}
+
+function genItems(){
+	return 2;
+}
