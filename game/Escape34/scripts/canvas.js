ca7cad626ec83796ae7593b1472aa2a6a7f8a18e
Miss-Terra <ms.terra.h@gmail.com>
2016-05-10 16:22:24 -0700

    fixed window scale ratio

--- game/Escape34/scripts/canvas.js
+++ game/Escape34/scripts/canvas.js
@@ -4,21 +4,46 @@
 	var
 	canvas,
 	ctx,
-	width,
-	height;
+	winWidth = $(window).width(),
+	winHeight = $(window).height();
 
 	canvas = document.getElementById("canvas")
 	ctx = canvas.getContext("2d");
 	
-	//Set the canvas width to your window width.
-	canvas.width = $(window).width();
-	canvas.height = $(window).height();
+	//resize canvas during initialization.
+	resizeCanvas();
+
+	
 	
 
 	//(event)When the window is resized, this function runs.
 	$(window).resize(function() {
-		canvas.width = $(window).width();
-		canvas.height = $(window).height();
-		
+		resizeCanvas();
 	});
+
+	//function that resizes canvas.
+	function resizeCanvas(){
+		winWidth = $(window).width(),
+		winHeight = $(window).height();
+		var heightMin = winWidth * (3/4);
+		var widthMin = winHeight * (4/3);
+
+			//Check if device is mobile or not.
+		if (winWidth < 800 && winHeight < 550){
+			//Set the canvas width to your window width.
+			canvas.width = winWidth;
+			canvas.height = winHeight;
+			console.log("isMobile")
+		}else{ // if desktop
+			//Settings to fit he screen, while keeping ratio.
+				if (heightMin <= winHeight){
+					canvas.width = winWidth;
+					canvas.height = heightMin;
+				}else{
+					canvas.width = widthMin;
+					canvas.height = winHeight;
+				}
+		}
+	}
 	
+
