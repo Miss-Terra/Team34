14239095b5cd6bf696179d101558de3670c346bb
Dan <zhudan_judy@aliyun.com>
2016-05-10 15:17:50 -0700

    Added loadMainMenu() and drawMainMenu() in screenMainMenu.js file

--- game/Escape34/scripts/screenMainMenu.js
+++ game/Escape34/scripts/screenMainMenu.js
@@ -1,24 +1,46 @@
-//function drawBackgroundImg(){
-////		bgImage.src = "images/???.png";
-//}
+function loadMainMenu() {
+	
+   // Judy: main menu background image		
+   bgImage.src = "images/mainMenu.jpg";
+   buttonImage.src = "images/button.png";       
+}
 
 function drawMainMenu(){
-	ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
-	
-//	var buttonWidth = canvas.width / 6;
-//	var buttonHeight = canvas.height / 10;	
-//	var	buttonY = canvas.height - (buttonHeight * 2);
-//	//Button x positions
-//	var restartX = (canvas.width / 2) - (buttonWidth / 2);
-//	var menuX = (restartX / 2) - (buttonWidth / 2);
-//	var nextX = restartX + (restartX / 2) + (buttonWidth / 2);
 
-//	ctx.drawImage(buttonImage, restartX, buttonY, buttonWidth, buttonHeight);
-//	uiObjects[0] = new uiObject(restartX, buttonY, buttonWidth, buttonHeight, 
-//			function (){
-//				console.log("restart");
-//				setState(1);
-//				console.log("Level: " + currentLevel)
-//			});
+	ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
+
+    var buttonWidth = canvas.width / 6;
+	var buttonHeight = canvas.height / 10;	
+	var	buttonY = canvas.height - (buttonHeight * 2);
+	//Button x positions
+	var scoreboardX = (canvas.width / 2) - (buttonWidth / 2);
+	var restartX = (scoreboardX / 2) - (buttonWidth / 2);            
+	var CreditsX = scoreboardX + (scoreboardX / 2) + (buttonWidth / 2); 
+
+    // middle button
+	ctx.drawImage(buttonImage, scoreboardX, buttonY, buttonWidth, buttonHeight);
+	uiObjects[0] = new uiObject(scoreboardX, buttonY, buttonWidth, buttonHeight, 
+			function (){
+				console.log("scoreboard");
+				setState(4);
+			});
+	
+    // left button
+	ctx.drawImage(buttonImage, restartX, buttonY, buttonWidth, buttonHeight);
+	uiObjects[1] = new uiObject(restartX, buttonY, buttonWidth, buttonHeight, 
+			function (){
+				console.log("restart");
+				setState(1);
+				console.log("Level: " + currentLevel)
+			});
+
+	
+    // right button
+	ctx.drawImage(buttonImage, CreditsX, buttonY, buttonWidth, buttonHeight);
+	uiObjects[2] = new uiObject(CreditsX, buttonY, buttonWidth, buttonHeight,
+			function () {
+				console.log("credits");
+				setState(5);
+			});
 
 }
