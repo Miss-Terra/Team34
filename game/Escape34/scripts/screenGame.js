d772cc72d3bf0f869b4843b18dff9b601e92d2b5
Miss-Terra <ms.terra.h@gmail.com>
2016-05-10 18:19:50 -0700

    fixed

--- game/Escape34/scripts/screenGame.js
+++ game/Escape34/scripts/screenGame.js
@@ -1,11 +1,11 @@
 function drawBackgroundImg(){
 		bgImage.src = "images/backgframe_480.jpg";
-<<<<<<< HEAD
+//<<<<<<< HEAD
   	buttonImage.src = "images/button.png";
   	initGameSetting(); //The setting required for gameplay. (run once)
-=======
+//=======
   	buttonImage.src = "images/gmenub1.png";
->>>>>>> 6b1deac05f70e8ef53ca0d4f671f6a3803f666d7
+//>>>>>>> 6b1deac05f70e8ef53ca0d4f671f6a3803f666d7
 }
 
 function drawGame(){
