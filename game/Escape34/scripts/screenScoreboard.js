12634fd6f71dbb56aa7d5147dd16f6fe75e801e1
Dan <zhudan_judy@aliyun.com>
2016-05-13 11:46:37 -0700

    Updated main menu button location in Scoreboard screen

--- game/Escape34/scripts/screenScoreboard.js
+++ game/Escape34/scripts/screenScoreboard.js
@@ -12,8 +12,7 @@ function drawScoreboard(){
 	var buttonHeight = canvas.height / 10;	
 	var	buttonY = canvas.height - (buttonHeight * 2);
 	//Button x positions
-	var menuX = (canvas.width / 2) - (buttonWidth / 2);
-	var creditsX = (menuX / 2) - (buttonWidth / 2);
+	var menuX = (canvas.width / 2) - (buttonWidth / 2) + ((canvas.width / 2) - (buttonWidth / 2)) / 2 + (buttonWidth / 2);  
 
 	ctx.drawImage(mainMenuButtonImage, menuX, buttonY, buttonWidth, buttonHeight);
 	uiObjects[0] = new uiObject(menuX, buttonY, buttonWidth, buttonHeight, 
