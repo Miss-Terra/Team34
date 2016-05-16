7cf4ba69591f0c0d07befe4f523140830b0a5279
Miss-Terra <ms.terra.h@gmail.com>
2016-05-16 15:38:42 -0700

    Line S shape

--- game/scripts/drawLine.js
+++ game/scripts/drawLine.js
@@ -9,7 +9,12 @@ function drawLine(){
 
 	var lineXStart = canvas.width * 0.85;
 	var lineYStart = canvas.height * 0.675;
-	var lineSpacing = personWidth + personWidth / 8 ;
+	var lineSpacingX = personWidth + personWidth / 8 ;
+	var lineSpacingY = personHeight + personHeight / 6 ;
+	var direction = "west";
+	var xMove = 0;
+	var yMove = 0;
+	var counter = 0;
 
 	//This displays the line of people and their clothes.
 	//maxPeopleInLine is defined in levels.js
@@ -17,14 +22,34 @@ function drawLine(){
 
 		//maxPeopleInLine in levels.js
 		//Make sure only a specific number people can show at one time.
-		if (i < maxPeopleInLine && personArray.length != 0){
+		if (i < maxPeopleInLine && personArray.length != 0 && i < 22){
 			personArray[i].setSize(personWidth, personHeight); // assign size variables to each person.
-			personArray[i].setPosition(lineXStart - (i*(lineSpacing)), lineYStart); // assign x and y variables to each person.
+			
+			if (i == 12){direction = "north"; counter = 1; lineXStart = personArray[i-1].x;}
+			else if (i == 14){direction = "east"; counter = 1; lineYStart = personArray[i-1].y;}
+			else if (i == 21){direction = "north"; counter = 1; lineXStart = personArray[i-1].x;}
 
-			//Always within canvas x borders
-			if (personArray[i].x > canvas.width - personArray[i].width || personArray[i].x < 0){
-				
-			}else{
+
+
+			if (direction == "west"){
+				xMove = -1;
+				yMove = 0;
+			}else if (direction == "east"){
+				xMove = 1;
+				yMove = 0;
+			}else if (direction == "north"){
+				yMove = -1;
+				xMove = 0;
+			}else if (direction == "south"){
+				yMove = 1;
+				xMove = 0;
+			}
+
+
+
+			personArray[i].setPosition(lineXStart + ((counter*xMove)*(lineSpacingX)), lineYStart + ((counter*yMove)*(lineSpacingY))); // assign x and y variables to each person.
+			
+
 
 				ctx.drawImage(personArray[i].image, personArray[i].x, personArray[i].y, personArray[i].width, personArray[i].height);
 				// Draws uiObjects for people.
@@ -41,8 +66,9 @@ function drawLine(){
 			 	//drawItems.js
 			 	//Draw the items for person
 			 	drawItems(personArray[i], false);
-			}
+			
 		}
+		counter++;
 	}
 }
 
