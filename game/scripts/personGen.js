69fc0de5f600dc1d9257fdaa0bf1671094594b4a
jade303 <tomjmckay@hotmail.ca>
2016-05-17 12:07:37 -0700

    small tweaks to item gen

--- game/scripts/personGen.js
+++ game/scripts/personGen.js
@@ -18,8 +18,8 @@ function genPerson(num, personImg){
 	var person = {
 		hat: 0,
 		shirt: 0,
-		item1: 0,
-		item2: 0,
+		itemf: 0,
+		itemb: 0,
 		pants: 0,
 		shoes: 0,
 		image: 0,
@@ -35,8 +35,8 @@ function genPerson(num, personImg){
 		setItems: function() {
 			this.hat = hatArray[Math.floor((Math.random() * 15) + 1)];
 			this.shirt = shirtArray[Math.floor((Math.random() * 12) + 1)];
-			this.item1 = itemfArray[Math.floor((Math.random() * 9) + 1)];
-			this.item2 = itembArray[Math.floor((Math.random() * 9) + 1)];
+			this.itemf = itemfArray[Math.floor((Math.random() * 9) + 1)];
+			this.itemb = itembArray[Math.floor((Math.random() * 9) + 1)];
 			this.pants = pantArray[Math.floor((Math.random() * 12) + 1)];
 			this.shoes = shoeArray[Math.floor((Math.random() * 9) + 1)];
 
@@ -55,8 +55,8 @@ function genPerson(num, personImg){
 		debugPerson: function() {
 			console.log("Hat: " + this.hat.src);
 			console.log("Shirt: " + this.shirt.src);
-			console.log("Item1: " + this.item1.src);
-			console.log("Item2: " + this.item2.src);
+			console.log("ItemFront: " + this.itemf.src);
+			console.log("ItemBack: " + this.itemb.src);
 			console.log("Pants: " + this.pants.src);
 			console.log("Shoes: " + this.shoes.src);
 		},
