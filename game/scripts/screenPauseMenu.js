cff6c659dac7edcf7f43cad2aaaebc70b972a61b
YooHyunsoo <fiypig89@gmail.com>
2016-05-19 15:23:25 -0700

    screenPauseMenu.js changed size of buttons

--- game/scripts/screenPauseMenu.js
+++ game/scripts/screenPauseMenu.js
@@ -9,7 +9,7 @@ function loadPauseMenu() {
 
 function drawPauseMenu() {
   ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
-  var buttonWidth = canvas.width / 6;
+  var buttonWidth = canvas.width / 10;
 	var buttonHeight = canvas.height / 10;	
 	var	buttonY = canvas.height - (buttonHeight * 2);
 	//Button x positions
@@ -75,7 +75,7 @@ function loadConfirmMenu() {
 function drawConfirmMenu() {
     ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
 
-    var buttonWidth = canvas.width / 6;
+    var buttonWidth = canvas.width / 10;
 	var buttonHeight = canvas.height / 10;	
 	var	buttonY = canvas.height - (buttonHeight * 2);
 	//Button x positions
