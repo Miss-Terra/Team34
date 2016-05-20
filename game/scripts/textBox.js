d98eb663d8b344c8b6c9ef22ce9f94af14d1272b
jade303 <tomjmckay@hotmail.ca>
2016-05-19 21:43:54 -0700

    center and resize text box

--- game/scripts/textBox.js
+++ game/scripts/textBox.js
@@ -8,12 +8,12 @@ function drawInputField() {
 	
 		   	inputField = new CanvasInput({
 					canvas: document.getElementById('canvas'),
-					x: canvas.width / 2,
+					x: canvas.width / 4,
 					y: canvas.height / 2,
 					fontSize: 18, 
 					fontColor: 'black',
 					fontWeight: 'bold',
-					width: 200,
+					width: canvas.width/2,
 					padding: 10,
 					borderWidth: 2
 			});
@@ -22,6 +22,11 @@ function drawInputField() {
 			console.log("inputField drawn");
 }
 function updateInputField(){
+	
+	inputField.x(canvas.width/4);
+	inputField.y(canvas.height/2);
+	inputField.width(canvas.width/2);
+
 	playerName = inputField.value();
 	inputField.render();
 	console.log("inputField rendered");
