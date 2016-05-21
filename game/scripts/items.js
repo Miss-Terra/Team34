5340fff3b173168f0d50c2d3c9782ce8e23f57ad
jade303 <tomjmckay@hotmail.ca>
2016-05-21 15:38:08 -0700

    created item object, methods and newItemArray

--- game/scripts/items.js
+++ game/scripts/items.js
@@ -4,6 +4,7 @@ var shirtArray = new Array();
 var pantArray = new Array();
 var shoeArray = new Array();
 var itembArray = new Array();
+var newItemArray = new Array();
 
 //Loads items into appropriate arrays, based on Number(type) in globals.js
 function loadItems() {
@@ -34,6 +35,43 @@ function loadItems() {
 			itembArray[((f-1)*NumberItems) + i].src = ("images/items/itemb/itemb_" + i + f + ".png");
 		}
 	}
+	///////////////////////newItemArray//////////////////////
+	for (g=0; g<5; g++) {
+
+		for (f=1; f<=NumberColors; f++) {
+			g=0;
+			for (i=1; i<=NumberItems; i++) {
+				newItemArray[g+(((f-1)*NumberItems) + i)] = new Image();
+				newItemArray[g+((f-1)*NumberItems) + i].src = ("images/items/itemf/itemf_" + i + f + ".png");
+			} 
+			g+= NumberColors*NumberItems;
+			for (i=1; i<=NumberHats; i++) {
+				newItemArray[g+((f-1)*NumberHats) + i] = new Image();
+				newItemArray[g+((f-1)*NumberHats) + i].src = ("images/items/hat/hat_" + i + f + ".png");
+			} 
+			g+= NumberColors*NumberHats;
+			for (i=1; i<=NumberShirts; i++) {
+				newItemArray[g+((f-1)*NumberShirts) + i] = new Image();
+				newItemArray[g+((f-1)*NumberShirts) + i].src = ("images/items/shirt/shirt_" + i + f + ".png");
+			} 
+			g+= NumberColors*NumberShirts;
+			for (i=1; i<=NumberPants; i++) {
+				newItemArray[g+((f-1)*NumberPants) + i] = new Image();
+				newItemArray[g+((f-1)*NumberPants) + i].src = ("images/items/pant/pant_" + i + f + ".png");
+			}
+			g+= NumberColors*NumberPants;
+			for (i=1; i<=NumberShoes; i++) {
+				newItemArray[g+((f-1)*NumberShoes) + i] = new Image();
+				newItemArray[g+((f-1)*NumberShoes) + i].src = ("images/items/shoe/shoe_" + i + f + ".png");
+			}
+			g+= NumberColors*NumberShoes;
+			for (i=1; i<=NumberItems; i++) {
+				newItemArray[g+((f-1)*NumberItems) + i] = new Image();
+				newItemArray[g+((f-1)*NumberItems) + i].src = ("images/items/itemb/itemb_" + i + f + ".png");
+			}
+		}
+	}
+	//////////////////////////////////
 }
 loadItems();
 
@@ -66,6 +104,9 @@ var Itm = {
 			} else {
 				
 			}
+	},
+	drawItmIndex: function(index, x, y) {
+		ctx.drawImage(newItemArray[index], x, y);
 	}
 }
 
@@ -81,3 +122,82 @@ var Itm = {
 //	this.type = Itmtype;
 //	this.number = Itmnumber;
 //	this.color = Itmcolor;
+
+
+
+//Begin new code
+
+//Generate n item objects and return an array of size n.
+function genItems(n) {
+	var genItemArray[];
+	
+	for (i=0; i<n; i++) {
+		genItemArray[i] = new item();
+	}
+	return genItemArray;
+}
+
+////////New item Object
+var item = {
+	//CONSTANTS
+	ITEMWIDTH:62,
+	ITEMHEIGHT:140,
+	
+	//Attributes (0 indicates that nothing was assigned!)
+	
+	// Item type 1-6 is itemf/hat/shirt/pants/shoes/itemb
+	type:0,
+	// Item numb is variation of type
+	numb:0,
+	// Item color is the color of item
+	color:0,
+	imgindex:0, //Each item has a pointer to an image file
+	imgsrc:null, 
+	
+	//Method to draw an item
+	draw: function() {
+		ctx.drawImage(newItemArray[this.imgsrc], x, y, ITEMWIDTH, ITEMHEIGHT);
+	},
+	compareColor: function(item) {
+		return (this.color == item.color); 
+	},
+	compareNumber: function(item) {
+		return (this.numb == item.numb); 
+	},
+	setItemAttr: function(t,n,c) {
+		this.type = t;
+		this.numb = n;
+		this.color = c;
+	},
+	setIndex: function() {
+		var temp =0;
+		
+		switch (this.type) {
+			case 6:
+				temp+= (NumberItems*NumberColors);
+			case 5:
+				temp+= (NumberShoes*NumberColors);
+			case 4:
+				temp+= (NumberPants*NumberColors);
+			case 3:
+				temp+= (NumberShirts*NumberColors);
+			case 2:
+				temp+= (NumberItems*NumberColors);
+			case 1:
+				break;
+				
+				temp+=((this.numb-1)*NumberColors + this.color) ;
+				this.imgindex = temp;
+		}
+		
+//		this.imgindex = ((t-1) * NumberColors*fx) + ((n-1) * NumberColors) + (c);
+		//where fx is simply the number of Variations (numb) assuming ALL items
+		// have the same number of variations
+	}
+};
+
+
+	
+}
+
+	//var x7 = function(){};
